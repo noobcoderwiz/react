3826df6ff1e7193a99545a2e78aa2e8ba39ded29
noobcoderwiz <vikarnsingh@mail.com>
2021-04-14 03:11:44 +0530

    confirmed working

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to React Vikarn test for deployment
+          Confirmer working for Vikarn
         </a>
       </header>
     </div>
