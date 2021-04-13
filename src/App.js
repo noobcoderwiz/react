16c1b3ab2056f0e66d4eb6d63ef9e065b6c2befc
Vikarn <vikarnsingh@gmail.com>
2021-04-14 03:03:21 +0530

    added deployment test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to React Vikarn
+          Welcome to React Vikarn test for deployment
         </a>
       </header>
     </div>
